3bfb400e00b24c109cd040df3ea1352c323ce919
gregorymark254 <gregorymark254@gmail.com>
2024-03-19 21:52:35 +0300

    teschohydro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './pages/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
+import Footer from './pages/Footer'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/gallery' element={<Gallery/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
